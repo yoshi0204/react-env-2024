6780195350920ae89f861f5e4e000f439774d21c
末永　陽士 <suenagayoshi0@gmail.com>
2024-01-17 18:20:40 +0900

    mod

---
+++ src/components/TextInput.test.tsx
@@ -0,0 +1,15 @@
+import { render, screen } from '@testing-library/react';
+import Textinput from './Textinput';
+import { userEvent } from '@testing-library/user-event';
+
+test('TextInput Conponent Test', async () => {
+  const user = userEvent.setup();
+  render(<Textinput />);
+
+  const textElement = screen.getByText('Entered Text:');
+  expect(textElement).toBeInTheDocument();
+
+  const inputElement = screen.getByLabelText('Text Input');
+  await user.type(inputElement, 'Hello World');
+  expect(screen.getByText('Entered Text: Hello World')).toBeInTheDocument();
+});
